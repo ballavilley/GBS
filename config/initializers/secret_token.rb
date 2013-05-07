c1b04bc48da9394a434f0d11288eec6ba43eea9b
Rhi Gemmell <rhigemmell@gbs-limited.co.uk>
2013-05-07 14:16:20 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GBS::Application.config.secret_token = 'bb6bba68044d9006152f00566c0e408fc74a4d141221d3088858841b08937c563f6096276d013ed91ee958db6522fbe752e6be1f83893c3f5fe24cfd1381ff0f'
